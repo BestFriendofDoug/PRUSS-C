e30202c9f1e3df4dcfdb3a97743183096923c3b2
root <root@beaglebone.(none)>
2015-03-06 05:27:44 +0000

    run file

---
+++ run.sh
@@ -0,0 +1,13 @@
+cd /PRUSS-C/PRUSS_LIB/example
+make
+mv data.bin text.bin /root/PRUSS-C/am335x_pru_package/pru_sw/example_apps
+cd ../../am335x_pru_package/pru_sw/example_apps
+make
+cd blinkled
+make
+modprobe uio_pruss 
+export LD_LIBRARY_PATH=/usr/local/lib
+mv /root/PRUSS-C/overlay/KA-GPIO-Test-00A0.dtbo /lib/firmware 
+echo KA-GPIO-Test:00A0 > /sys/devices/bone_capemgr.9/slots
+cd ../bin
+./blinkled​
